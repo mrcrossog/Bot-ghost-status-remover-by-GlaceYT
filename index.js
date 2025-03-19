54583711c3a363630ce3c6e51fba64d9dd52ad99
MrCross_og <71199236+mrcrossog@users.noreply.github.com>
2025-03-19 21:08:03 +1000

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Наблюдает за ОЗ.", "🎮 Playing VALORANT"];
+const statusMessages = ["Наблюдает за ОЗ.", "Играет на ОЗ."];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
